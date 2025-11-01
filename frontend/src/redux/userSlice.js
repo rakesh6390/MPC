a3c66ba127ad1e5492e88f6a9d9faa1b60370734
rakesh6390 <it12212035@gmail.com>
2025-11-01 23:07:56 +0530

    update

---
+++ frontend/src/redux/userSlice.js
@@ -0,0 +1,15 @@
+
+
+const userSlice = createSlice({
+    name: user,
+    initialState:{
+        userData:null
+    },
+    reducers:{
+        setUserData:(state,action)=>{
+            state.userData= action.payload
+        }
+    }
+})
+export const {setUserData} = userSlice.actions
+export default userSlice.reducer
