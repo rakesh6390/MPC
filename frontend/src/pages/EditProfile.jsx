5c15cdda375aa78860db8678863bdb7301d80028
rakesh6390 <it12212035@gmail.com>
2026-01-03 01:13:42 +0530

    updates

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -1,8 +1,114 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {serverUrl} from "../App"
+import axios from 'axios'
+import { setUserData } from '../redux/userSlice'
+import { ClipLoader } from 'react-spinners'
+import { FaArrowLeftLong } from 'react-icons/fa6'
+import { useDispatch, useSelector } from 'react-redux'
+import { toast } from 'react-toastify'
+
+import { useNavigate } from 'react-router-dom'
 
 function EditProfile() {
+  const navigate = useNavigate()
+  const { userData } = useSelector(state => state.user)
+  let [name ,setName] = useState(userData?.name || "")
+  let [description , setDescription] = useState(userData?.description || "")
+  let [photoUrl,setPhotoUrl] = useState(null)
+  let dispatch = useDispatch()
+  let [loading ,setLoading ] = useState(false)
+  
+  const formData= new FormData()
+  formData.append("name",name)
+  formData.append("description",description)
+  formData.append("photoUrl",photoUrl)
+ 
+ const handleEditProfile = async ()=>{
+  setLoading(true)
+  try {
+    const result = await axios.post(serverUrl+"/api/user/profile",formData,{withCredentials:true})
+    console.log(result.data)
+    dispatch(setUserData(result.data))
+    setLoading(false)
+    navigate("/")
+    
+
+    toast.success("Profile Updated Successfully")
+  } catch (error) {
+    setLoading(false)
+    console.log(error)
+    toast.error(error.response.data.message)
+  }
+ }
+
   return (
-    <div>EditProfile</div>
+    <div className='min-h-screen flex items-center justify-center bg-gray-100 px-4 py-10'>
+      <div className='bg-white rounded-2xl shadow-lg p-8 max-w-xl w-full relative'>
+        
+        <FaArrowLeftLong
+          className='absolute top-[5%] left-[5%] w-[22px] h-[22px] cursor-pointer'
+          onClick={() => navigate("/profile")}
+        />
+          <h2 className='flex items-center justify-center text-2xl font-bold text-gray-800 mb-6'> Edit Profile </h2>
+
+
+        <form action="" className="space-y-5" onSubmit={(e)=>e.preventDefault()}>
+  
+          <div className="flex flex-col items-center text-center">
+            {userData?.photoUrl ? (
+              <img
+                src={userData.photoUrl}
+                className="w-24 h-24 rounded-full object-cover border-4 border-black"
+                alt="profile"
+              />
+            ) : (
+              <div className='w-24 h-24 rounded-full text-white flex items-center justify-center text-[30px] border-2 bg-black border-white cursor-pointer'>
+                {userData?.name?.slice(0, 1).toUpperCase()}
+              </div>
+            )}
+          </div>
+        <div> 
+          <label htmlFor="image" className='text-sm font-medium text-gray-700'>Select Avtar</label>
+
+         <input id='image' type="file" name ="photoUrl" accept="image/*"
+          className='w-full px-4 py-2  border rounded-md text-sm' onChange={(e)=>
+            setPhotoUrl(e.target.files[0])
+          }/>
+         </div>
+
+           <div> 
+          <label htmlFor="name" className='text-sm font-medium text-gray-700'>Username</label>
+
+         <input id='name' type="text" placeholder={userData?.name}
+          className='w-full px-4 py-2  border rounded-md text-sm' onChange={(e)=>setName(e.target.value)} value={name}/>
+         </div>
+
+         <div> 
+          <label  className='text-sm font-medium text-gray-700'>Email</label>
+
+         <input type="email" readOnly placeholder={userData?.email}
+          className='w-full px-4 py-2  border rounded-md text-sm'/>
+         </div>
+
+         <div>
+          <label className="text-sm font-medium text-gray-700">Description</label>
+          <textarea name="description" className="w-full mt-1 px-4 py-2 border border-gray-300 rounded-md resize-none focus:ring-2 focus-ring-[black]" 
+          rows={3}
+          placeholder="Tell us about yourself"
+          onChange={(e)=>setDescription(e.target.value)} value={description}
+          ></textarea>
+         </div>
+
+         {/*Save button*/}
+         <button type="submit"
+          className='w-full bg-black active:bg-[#454545] text-white py-2 rounded-md font-medium transition cursor-pointer' onClick={handleEditProfile}>
+          {loading? <ClipLoader size={30} color='white'/>: "Save Changes"}
+         </button>
+
+        </form>
+
+      </div>
+    </div>
   )
 }
 
